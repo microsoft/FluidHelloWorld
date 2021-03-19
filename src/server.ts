d44e8f33df76cf557fc5006d9778907465a9a7af
Tom Laird-McConnell <tomlm@microsoft.com>
2021-03-19 16:30:00 -0700

    add comments

--- src/server.ts
+++ src/server.ts
@@ -10,20 +10,35 @@ import * as restify from 'restify';
 import { ActivityTypes, BotFrameworkAdapter, StatusCodes, WebRequest, WebResponse } from 'botbuilder';
 import { ActionContentType, AdaptiveCardAction, AdaptiveCardContentType } from './card';
 
-// Create HTTP server.
-const adapter = new BotFrameworkAdapter({ appId: process.env.MicrosoftAppId, appPassword: process.env.MicrosoftAppPassword });
+// create fluid service reference
 const service = new TinyliciousService();
+
+// create http server
 const server = restify.createServer();
+
+// listen on port
 server.listen(process.env.port || process.env.PORT || 3978, () => console.log(`\n${server.name} listening to ${server.url}`));
-// Listen for incoming requests.
+
+// adapter to use for validating requests.
+const adapter = new BotFrameworkAdapter({ appId: process.env.MicrosoftAppId, appPassword: process.env.MicrosoftAppPassword });
+
+// endpoint for invoke activity POST back for card actions
 server.post('/api/messages', (req: WebRequest, res: WebResponse) => {
+
     adapter.processActivity(req, res, async (context) => {
         if (context.activity.type == ActivityTypes.Invoke && context.activity.name == ActionContentType) {
+            // action payload from the card
             let action = <AdaptiveCardAction>context.activity.value.action;
+
+            // get the app
             var fluidContainer = await Fluid.getContainer(service, <string>action.id, [DiceApp]);
             let diceApp = await fluidContainer.getDataObject('kvpairId');
-            let card = await diceApp.onAction(action);
-            await context.sendActivity({ type: "invokeResponse", value: { status: StatusCodes.OK, body: { statusCode: StatusCodes.OK, type: AdaptiveCardContentType, data: card } } });
+            
+            // invoke the action logic
+            let updatedCard = await diceApp.onAction(action);
+
+            // send updated card
+            await context.sendActivity({ type: "invokeResponse", value: { status: StatusCodes.OK, body: { statusCode: StatusCodes.OK, type: AdaptiveCardContentType, data: updatedCard } } });
         }
     });
 });
