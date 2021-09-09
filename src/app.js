aeed4d604c17b6422e67d8595f89791d74ed145e
Micah Godbolt <mgodbolt@microsoft.com>
2021-09-09 15:59:56 -0700

    remove additional views and move all docs to website (#163)
    * remove additional views and move all docs to website
    
    * Update src/app.js
    
    Co-authored-by: Tyler Butler <tyler@tylerbutler.com>
    
    * Update src/app.js
    
    Co-authored-by: Tyler Butler <tyler@tylerbutler.com>
    
    Co-authored-by: Tyler Butler <tyler@tylerbutler.com>

--- src/app.js
+++ src/app.js
@@ -5,9 +5,49 @@
 
 import { SharedMap } from "fluid-framework";
 import { TinyliciousClient } from "@fluidframework/tinylicious-client";
-import { jsRenderView as renderDiceRoller } from "./view";
 
 export const diceValueKey = "dice-value-key";
+
+
+// Create the view
+
+const template = document.createElement("template");
+
+template.innerHTML = `
+  <style>
+    .wrapper { text-align: center }
+    .dice { font-size: 200px }
+    .roll { font-size: 50px;}
+  </style>
+  <div class="wrapper">
+    <div class="dice"></div>
+    <button class="roll"> Roll </button>
+  </div>
+`
+
+const renderDiceRoller = (diceMap, elem) => {
+    elem.appendChild(template.content.cloneNode(true));
+
+    const rollButton = elem.querySelector(".roll");
+    const dice = elem.querySelector(".dice");
+
+    // Set the value at our dataKey with a random number between 1 and 6.
+    rollButton.onclick = () => diceMap.set(diceValueKey, Math.floor(Math.random() * 6) + 1);
+
+    // Get the current value of the shared data to update the view whenever it changes.
+    const updateDice = () => {
+        const diceValue = diceMap.get(diceValueKey);
+        // Unicode 0x2680-0x2685 are the sides of a dice (⚀⚁⚂⚃⚄⚅)
+        dice.textContent = String.fromCodePoint(0x267f + diceValue);
+        dice.style.color = `hsl(${diceValue * 60}, 70%, 50%)`;
+    };
+    updateDice();
+
+    // Use the changed event to trigger the rerender whenever the value changes.
+    diceMap.on("valueChanged", updateDice);
+}
+
+// Load container and render view
 
 const client = new TinyliciousClient();
 const containerSchema = {
