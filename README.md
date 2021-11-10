3aaf86f4fd9231ddb9d2575d5411a4f62d8f8b49
Serge van den Oever <svdoever@gmail.com>
2021-11-10 12:27:58 +0100

    Simplistic sequence implementation

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # @fluid-example/hello-world
 
-This repository contains a simple app that enables all connected clients to roll a dice and view the result. For a
+This repository is based on the simple app example [FluidHelloWorld](https://github.com/microsoft/FluidHelloWorld)
+of Microsoft that enables all connected clients to roll a dice and view the result. 
+
+For a
 walkthrough of this example and how it works, check out the [tutorial documentation](https://aka.ms/fluid/tutorial).
+
+Purpose of this fork is to experiment with recording and sharing the sequence of dice values over all connected clients.
+
+![Roll dice and show sequence](README_artifacts/RollDiceWithSequence.png)
+The first naive implementation is by 
 
 ## Requirements
 
--- src/app.js
+++ src/app.js
@@ -4,42 +4,105 @@
  */
 
 import { SharedMap } from "fluid-framework";
-import { TinyliciousClient } from "@fluidframework/tinylicious-client";
+import { AzureClient, LOCAL_MODE_TENANT_ID } from "@fluidframework/azure-client";
+import { InsecureTokenProvider } from "@fluidframework/test-client-utils";
 
-export const diceValueKey = "dice-value-key";
-
-// Load container and render the app
-
-const client = new TinyliciousClient();
-const containerSchema = {
-    initialObjects: { diceMap: SharedMap }
+// The config is set to run against a local service by default. Run `npx tinylicious` to run locally
+// Update the corresponding properties below with your tenant specific information to run against your tenant.
+const serviceConfig = {
+    connection: {
+        tenantId: LOCAL_MODE_TENANT_ID, // REPLACE WITH YOUR TENANT ID
+        tokenProvider: new InsecureTokenProvider("" /* REPLACE WITH YOUR PRIMARY KEY */, {
+            id: "userId",
+        }),
+        orderer: "http://localhost:7070", // REPLACE WITH YOUR ORDERER ENDPOINT
+        storage: "http://localhost:7070", // REPLACE WITH YOUR STORAGE ENDPOINT
+    },
 };
+
+const client = new AzureClient(serviceConfig);
+
+const diceValueKey = "dice-value-key";
+const diceValuesKey = "dice-values-key";
+
+const containerSchema = {
+    initialObjects: {
+        diceMap: SharedMap,
+    },
+};
+
+// Log message as HTML to 'log' div element
+const log = (message) => {
+    const root = document.getElementById("content");
+    const logElement = root.querySelector(".log");
+    logElement.innerHTML += "<br/>" + message;
+};
+
+// Log message as HTML to 'log' div element
+const clearLog = () => {
+    const root = document.getElementById("content");
+    const logElement = root.querySelector(".log");
+    logElement.innerHTML = "";
+};
+
+// Record the dice value both as single latest value, and in the sequence of values
+const recordDiceValue = (container, value) => {
+    container.initialObjects.diceMap.set(diceValueKey, value);
+    let diceValues = container.initialObjects.diceMap.get(diceValuesKey);
+    if (!diceValues) {
+        diceValues = [value];
+    } else {
+        diceValues.push(value);
+    }
+    container.initialObjects.diceMap.set(diceValuesKey, diceValues);
+};
+
+// Clear the recorded sequence of dice values
+const clearDiceValues = (container) => {
+    container.initialObjects.diceMap.set(diceValuesKey, []);
+};
+
+const getDiceValue = (container) => {
+    return container.initialObjects.diceMap.get(diceValueKey);
+};
+
+const getDiceValuesAsText = (container) => {
+    const diceValues = container.initialObjects.diceMap.get(diceValuesKey);
+    if (!diceValues) {
+        return "<no values yet>";
+    } else {
+        const diceValuesAsText = diceValues.map((value) => value.toString()).join(" ");
+        return diceValuesAsText;
+    }
+};
+
 const root = document.getElementById("content");
 
 const createNewDice = async () => {
     const { container } = await client.createContainer(containerSchema);
-    container.initialObjects.diceMap.set(diceValueKey, 1);
+    recordDiceValue(container, 1);
     const id = await container.attach();
-    renderDiceRoller(container.initialObjects.diceMap, root);
+    renderDiceRoller(container, root);
+
     return id;
-}
+};
 
 const loadExistingDice = async (id) => {
     const { container } = await client.getContainer(id, containerSchema);
-    renderDiceRoller(container.initialObjects.diceMap, root);
-}
+    renderDiceRoller(container, root);
+};
 
 async function start() {
     if (location.hash) {
-        await loadExistingDice(location.hash.substring(1))
+        await loadExistingDice(location.hash.substring(1));
     } else {
         const id = await createNewDice();
         location.hash = id;
+        log(`New dice created with id ${id}`);
     }
 }
 
 start().catch((error) => console.error(error));
-
 
 // Define the view
 
@@ -49,32 +112,60 @@ template.innerHTML = `
   <style>
     .wrapper { text-align: center }
     .dice { font-size: 200px }
+    .dicevaluesContainer { font-size: 24px; border: 3px solid black; padding: 10px; margin: 10px; }
     .roll { font-size: 50px;}
+    .clear { font-size: 50px;}
+    .logContainer { font-size: 24px; border: 3px solid black; padding: 10px; margin: 10px }
   </style>
   <div class="wrapper">
     <div class="dice"></div>
+    <div class="dicevaluesContainer">
+      <strong>DICE VALUES</strong><hr/>
+      <div class="dicevalues"></div>
+    </div>
     <button class="roll"> Roll </button>
+    <button class="clear"> Clear dice values </button>
+    <div class="logContainer">
+      <strong>LOCAL LOG OUTPUT</strong><hr/>
+      <div class="log"></div>
+    </div>
   </div>
-`
+`;
 
-const renderDiceRoller = (diceMap, elem) => {
+const renderDiceRoller = (container, elem) => {
     elem.appendChild(template.content.cloneNode(true));
 
     const rollButton = elem.querySelector(".roll");
+    const clearButton = elem.querySelector(".clear");
     const dice = elem.querySelector(".dice");
+    const dicevaluesElement = elem.querySelector(".dicevalues");
 
     // Set the value at our dataKey with a random number between 1 and 6.
-    rollButton.onclick = () => diceMap.set(diceValueKey, Math.floor(Math.random() * 6) + 1);
+    rollButton.onclick = () => {
+        const value = Math.floor(Math.random() * 6) + 1;
+        recordDiceValue(container, value);
+        log(`You rolled a ${value}`);
+    };
+
+    // Clear the recorded values
+    clearButton.onclick = () => {
+        clearDiceValues(container);
+        clearLog();
+    };
 
     // Get the current value of the shared data to update the view whenever it changes.
     const updateDice = () => {
-        const diceValue = diceMap.get(diceValueKey);
+        const diceValue = getDiceValue(container);
+        const diceValuesAsText = getDiceValuesAsText(container);
+
         // Unicode 0x2680-0x2685 are the sides of a dice (⚀⚁⚂⚃⚄⚅)
         dice.textContent = String.fromCodePoint(0x267f + diceValue);
         dice.style.color = `hsl(${diceValue * 60}, 70%, 30%)`;
+
+        dicevaluesElement.textContent = diceValuesAsText;
     };
     updateDice();
 
     // Use the changed event to trigger the rerender whenever the value changes.
-    diceMap.on("valueChanged", updateDice);
-}
+    container.initialObjects.diceMap.on("valueChanged", updateDice);
+};
