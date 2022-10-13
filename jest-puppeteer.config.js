e62f3c49aacbd0a5dea640825a9a20b367032d8b
sdeshpande3 <sdeshpande@microsoft.com>
2022-10-13 15:20:53 -0700

    add args

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -6,12 +6,12 @@
 module.exports = {
   server: {
       command: `npm run start:client`,
-      launchTimeout: 500000
+      launchTimeout: 45000
   },
   launch: {
       slowMo: 30, // slows down process for easier viewing
       headless: false, // run in the browser
-      // args: ['--no-sandbox', '--disable-setuid-sandbox'], // https://github.com/puppeteer/puppeteer/blob/master/docs/troubleshooting.md#setting-up-chrome-linux-sandbox
-      // dumpio: process.env.FLUID_TEST_VERBOSE !== undefined // output browser console to cmd line
+      args: ['--no-sandbox', '--disable-setuid-sandbox'], // https://github.com/puppeteer/puppeteer/blob/master/docs/troubleshooting.md#setting-up-chrome-linux-sandbox
+      dumpio: process.env.FLUID_TEST_VERBOSE !== undefined // output browser console to cmd line
   },
 };
