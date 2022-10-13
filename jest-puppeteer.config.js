dee852046aa34cc76d93ecb5acb9e71aeed35184
sdeshpande3 <sdeshpande@microsoft.com>
2022-10-13 15:17:47 -0700

    headless to false

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -5,13 +5,13 @@
 
 module.exports = {
   server: {
-      command: `npm run start:server`,
+      command: `npm run start:client`,
       launchTimeout: 500000
   },
   launch: {
       slowMo: 30, // slows down process for easier viewing
-      headless: true, // run in the browser
-      args: ['--no-sandbox', '--disable-setuid-sandbox'], // https://github.com/puppeteer/puppeteer/blob/master/docs/troubleshooting.md#setting-up-chrome-linux-sandbox
-      dumpio: process.env.FLUID_TEST_VERBOSE !== undefined // output browser console to cmd line
+      headless: false, // run in the browser
+      // args: ['--no-sandbox', '--disable-setuid-sandbox'], // https://github.com/puppeteer/puppeteer/blob/master/docs/troubleshooting.md#setting-up-chrome-linux-sandbox
+      // dumpio: process.env.FLUID_TEST_VERBOSE !== undefined // output browser console to cmd line
   },
 };
