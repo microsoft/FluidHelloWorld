b1b41f61f7d1a496d5e017df778fc578ac5cc723
sdeshpande3 <sdeshpande@microsoft.com>
2022-10-14 20:22:29 -0700

    eslint wip

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,33 +4,18 @@
  */
 
 module.exports = {
-	"env": {
-		"browser": true,
-		"es2021": true
-	},
-	"extends": "eslint:recommended",
-	"overrides": [
-	],
-	"parserOptions": {
-		"ecmaVersion": "latest",
-		"sourceType": "module"
-	},
-	"rules": {
-		"indent": [
-			"error",
-			"tab"
-		],
-		"linebreak-style": [
-			"error",
-			"windows"
-		],
-		"quotes": [
-			"error",
-			"double"
-		],
-		"semi": [
-			"error",
-			"always"
-		]
-	}
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: "eslint:recommended",
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+  },
 };
--- src/app.js
+++ src/app.js
@@ -7,42 +7,40 @@ import { SharedMap } from "fluid-framework";
 import { TinyliciousClient } from "@fluidframework/tinylicious-client";
 
 export const diceValueKey = "dice-value-key";
-
 // Load container and render the app
 
 const client = new TinyliciousClient();
 const containerSchema = {
-    initialObjects: { diceMap: SharedMap }
+  initialObjects: { diceMap: SharedMap },
 };
+
 const root = document.getElementById("content");
 
 const createNewDice = async () => {
-    const { container } = await client.createContainer(containerSchema);
-    container.initialObjects.diceMap.set(diceValueKey, 1);
-    const id = await container.attach();
-    renderDiceRoller(container.initialObjects.diceMap, root);
-    return id;
-}
+  const { container } = await client.createContainer(containerSchema);
+  container.initialObjects.diceMap.set(diceValueKey, 1);
+  const id = await container.attach();
+  renderDiceRoller(container.initialObjects.diceMap, root);
+  return id;
+};
 
 const loadExistingDice = async (id) => {
-    const { container } = await client.getContainer(id, containerSchema);
-    renderDiceRoller(container.initialObjects.diceMap, root);
-}
+  const { container } = await client.getContainer(id, containerSchema);
+  renderDiceRoller(container.initialObjects.diceMap, root);
+};
 
 async function start() {
-    if (location.hash) {
-        await loadExistingDice(location.hash.substring(1))
-    } else {
-        const id = await createNewDice();
-        location.hash = id;
-    }
+  if (location.hash) {
+    await loadExistingDice(location.hash.substring(1));
+  } else {
+    const id = await createNewDice();
+    location.hash = id;
+  }
 }
 
 start().catch((error) => console.error(error));
 
-
 // Define the view
-
 const template = document.createElement("template");
 
 template.innerHTML = `
@@ -55,26 +53,27 @@ template.innerHTML = `
     <div class="dice"></div>
     <button class="roll"> Roll </button>
   </div>
-`
+`;
 
 const renderDiceRoller = (diceMap, elem) => {
-    elem.appendChild(template.content.cloneNode(true));
+  elem.appendChild(template.content.cloneNode(true));
 
-    const rollButton = elem.querySelector(".roll");
-    const dice = elem.querySelector(".dice");
+  const rollButton = elem.querySelector(".roll");
+  const dice = elem.querySelector(".dice");
 
-    // Set the value at our dataKey with a random number between 1 and 6.
-    rollButton.onclick = () => diceMap.set(diceValueKey, Math.floor(Math.random() * 6) + 1);
+  // Set the value at our dataKey with a random number between 1 and 6.
+  rollButton.onclick = () =>
+    diceMap.set(diceValueKey, Math.floor(Math.random() * 6) + 1);
 
-    // Get the current value of the shared data to update the view whenever it changes.
-    const updateDice = () => {
-        const diceValue = diceMap.get(diceValueKey);
-        // Unicode 0x2680-0x2685 are the sides of a dice (⚀⚁⚂⚃⚄⚅)
-        dice.textContent = String.fromCodePoint(0x267f + diceValue);
-        dice.style.color = `hsl(${diceValue * 60}, 70%, 30%)`;
-    };
-    updateDice();
+  // Get the current value of the shared data to update the view whenever it changes.
+  const updateDice = () => {
+    const diceValue = diceMap.get(diceValueKey);
+    // Unicode 0x2680-0x2685 are the sides of a dice (⚀⚁⚂⚃⚄⚅)
+    dice.textContent = String.fromCodePoint(0x267f + diceValue);
+    dice.style.color = `hsl(${diceValue * 60}, 70%, 30%)`;
+  };
+  updateDice();
 
-    // Use the changed event to trigger the rerender whenever the value changes.
-    diceMap.on("valueChanged", updateDice);
-}
+  // Use the changed event to trigger the rerender whenever the value changes.
+  diceMap.on("valueChanged", updateDice);
+};
