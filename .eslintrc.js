b96ff56eff48ad0621f3ac01034cb5201726809f
sdeshpande3 <sdeshpande@microsoft.com>
2022-10-17 12:12:40 -0700

    setup prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,17 +4,17 @@
  */
 
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: "eslint:recommended",
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  rules: {
-    quotes: ["error", "double"],
-    semi: ["error", "always"],
-  },
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: "eslint:recommended",
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+    rules: {
+        quotes: ["error", "double"],
+        semi: ["error", "always"],
+    },
 };
