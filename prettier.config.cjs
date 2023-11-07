a36d54faccffe29c3449f4f917ba6ffdd5793fd2
Josmithr <josmithr@microsoft.com>
2023-11-07 10:19:37 -0800

    tools: Update prettier config for yaml and re-run

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -5,4 +5,15 @@
 
 module.exports = {
 	...require("@fluidframework/build-common/prettier.config.cjs"),
+	overrides: [
+		{
+			// YAML formatting should not use tabs, and use a 2-space indent instead
+			files: ["*.yaml", "*.yml"],
+			options: {
+				tabWidth: 2,
+				useTabs: false,
+				quoteProps: "preserve",
+			},
+		},
+	],
 };
