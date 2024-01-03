ac45a53a25d052e8e6495aebb93a1e547b13f219
nmsimons <nick@dreamlarge.com>
2024-01-03 14:52:41 -0800

    update to single init function

--- src/app.js
+++ src/app.js
@@ -37,29 +37,27 @@ export const treeConfiguration = new TreeConfiguration(
 		}),
 );
 
-const root = document.getElementById("content");
+const content = document.getElementById("content");
 
-const createNewDice = async () => {
-	const { container } = await client.createContainer(containerSchema);
-	// Initialize the SharedTree Data Structure
-	const diceTree = container.initialObjects.diceTree.schematize(treeConfiguration);
-	const id = await container.attach();
-	renderDiceRoller(diceTree, root);
-	return id;
-};
-
-const loadExistingDice = async (id) => {
-	const { container } = await client.getContainer(id, containerSchema);
-	// Initialize the SharedTree Data Structure
-	const diceTree = container.initialObjects.diceTree.schematize(treeConfiguration);
-	renderDiceRoller(diceTree, root);
+export const loadFluidData = async (containerId, containerSchema) => {
+	// Get or create the document depending if we are running through the create new flow
+	if (containerId.length === 0) {
+		// The client will create a new detached container using the schema
+		// A detached container will enable the app to modify the container before attaching it to the client
+		return await client.createContainer(containerSchema);
+	} else {
+		// Use the unique container ID to fetch the container created earlier. It will already be connected to the
+		// collaboration session.
+		return await client.getContainer(containerId, containerSchema);
+	}
 };
 
 async function start() {
-	if (location.hash) {
-		await loadExistingDice(location.hash.substring(1));
-	} else {
-		const id = await createNewDice();
+	const { container } = await loadFluidData(location.hash.substring(1), containerSchema);
+	const diceTree = container.initialObjects.diceTree.schematize(treeConfiguration);
+	renderDiceRoller(diceTree.root, content);
+	if (!location.hash) {
+		const id = await container.attach();
 		location.hash = id;
 	}
 }
@@ -81,7 +79,7 @@ template.innerHTML = `
   </div>
 `;
 
-const renderDiceRoller = (diceTree, elem) => {
+const renderDiceRoller = (diceRoot, elem) => {
 	elem.appendChild(template.content.cloneNode(true));
 
 	const rollButton = elem.querySelector(".roll");
@@ -89,19 +87,19 @@ const renderDiceRoller = (diceTree, elem) => {
 
 	// Set the value at our dataKey with a random number between 1 and 6.
 	rollButton.onclick = () => {
-		diceTree.root.value = (diceValueKey, Math.floor(Math.random() * 6) + 1);
+		diceRoot.value = (diceValueKey, Math.floor(Math.random() * 6) + 1);
 	};
 
 	// Get the current value of the shared data to update the view whenever it changes.
 	const updateDice = () => {
 		// Unicode 0x2680-0x2685 are the sides of a dice (⚀⚁⚂⚃⚄⚅)
-		dice.textContent = String.fromCodePoint(0x267f + diceTree.root.value);
-		dice.style.color = `hsl(${diceTree.root.value * 60}, 70%, 30%)`;
+		dice.textContent = String.fromCodePoint(0x267f + diceRoot.value);
+		dice.style.color = `hsl(${diceRoot.value * 60}, 70%, 30%)`;
 	};
 	updateDice();
 
 	// Use the changed event to trigger the rerender whenever the value changes.
-	Tree.on(diceTree.root, "afterChange", () => updateDice());
+	Tree.on(diceRoot, "afterChange", () => updateDice());
 	// Setting "fluidStarted" is just for our test automation
 	window["fluidStarted"] = true;
 };
