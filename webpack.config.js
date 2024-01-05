f7253ddb8dcf849ed6f10fe638409598f8710df7
Matt Rakow <marakow@microsoft.com>
2024-01-04 17:08:32 -0800

    Prettier fix

--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = (env) => {
 	const htmlTemplate = "./src/index.html";
-	const plugins = [
-		new HtmlWebpackPlugin({ template: htmlTemplate }),
-	];
+	const plugins = [new HtmlWebpackPlugin({ template: htmlTemplate })];
 
 	const mode = env && env.prod ? "production" : "development";
 
