086aa3f9d35de5ca002d44efdf8657c3dfa097fe
nmsimons <nick@dreamlarge.com>
2024-04-15 14:51:13 -0700

    Update to rc3

--- src/app.js
+++ src/app.js
@@ -92,7 +92,7 @@ const renderDiceRoller = (dice, elem) => {
 	updateDice();
 
 	// Use the changed event to trigger the rerender whenever the value changes.
-	Tree.on(dice, "afterChange", updateDice);
+	Tree.on(dice, "treeChanged", updateDice);
 	// Setting "fluidStarted" is just for our test automation
-	window["fluidStarted"] = true;
+	window.fluidStarted = true;
 };
